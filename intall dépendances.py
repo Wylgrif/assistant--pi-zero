1dc9e31f4e42c5d7f9acd44d99231467b811f830
Wylgrif <164638876+Wylgrif@users.noreply.github.com>
2024-05-28 17:34:47 +0200

    Create intall dépendances.py

---
+++ intall dépendances.py
@@ -0,0 +1,5 @@
+import os
+os.system("pip   installer   RealtimeTTS")
+os.system("sudo pip3 install SpeechRecognition")
+os.system("sudo apt-get install espeak")
+os.system("sudo apt-get install python-espeak")
