96d320fad173cae6c7e7c1ec11aa1275a0490d36
Wylgrif <164638876+Wylgrif@users.noreply.github.com>
2024-12-15 13:00:41 +0100

    Update intall dépendances.py
    updatelinuxpakage

--- intall dépendances.py
+++ intall dépendances.py
@@ -1,4 +1,6 @@
 import os
+os.sysetm(sudo apt-get update)
+os.system(sudo apt-get upgrade)
 os.system("pip   installer   RealtimeTTS")
 os.system("sudo pip3 install SpeechRecognition")
 os.system("sudo apt-get install espeak")
